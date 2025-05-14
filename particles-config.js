9d4313d9ea179575c9ae4ce77bb00c0e621c6748
JakoJ-00 <rozer0902@gmail.com>
2025-05-14 19:48:57 +0200

    Create particles-config.js

---
+++ particles-config.js
@@ -0,0 +1,23 @@
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 80
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "circle"
+    },
+    "opacity": {
+      "value": 0.2
+    },
+    "size": {
+      "value": 2
+    },
+    "move": {
+      "enable": true,
+      "speed": 0.5
+    }
+  }
+});
